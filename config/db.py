2b647bbc27c2eaed8fb71424a604e0e802486428
Dafa <dafaaqilla72@gmail.com>
2024-02-20 07:58:01 +0700

    refactor: Rename db

--- config/db.py
+++ config/db.py
@@ -2,7 +2,6 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
 engine = create_engine(
-    "mysql+pymysql://root:@localhost:3306/ipc_management", )
+    "mysql+pymysql://root:@localhost:3306/b7_locator", )
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
